1478f9886aafe107725186ab4326e98e06267a21
jiangfanan <123@qq.com>
2020-05-06 11:22:00 +0800

    多态

---
+++ src/club/Shape.java
@@ -0,0 +1,20 @@
+package club;
+
+public class Shape {
+
+
+        public double area() {
+            return 0.0;
+        }
+
+        public double perimeter() {
+            return 0.0;
+        }
+
+        public String getShapeName() {
+            return "形状";
+        }
+
+
+    }
+
